9a500b85448ce1fbf288dd83656547ab6b72d333
Denis <denis.alekseev.200411@mail.ru>
2024-04-18 09:24:51 +0300

    lox

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Remoting.Channels;
+using System.Windows;
+
+namespace Shaitanet.View
+{
+    public partial class MainWindow : Window
+    {
+
+
+
+        public ObservableCollection<Service> ProductList
+        {
+            get { return (ObservableCollection<Service>)GetValue(ProductListProperty); }
+            set { SetValue(ProductListProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProductListProperty =
+            DependencyProperty.Register("ProductList", typeof(ObservableCollection<Service>), typeof(MainWindow));
+
+
+
+        public MainWindow()
+        {
+            ProductList = new ObservableCollection<Service>(App.shaitanetEntities.Service);
+
+            InitializeComponent();
+        }
+    }
+}
